966e5ead054421aee8a1fe1ffe75b4b77cc0ed35
Eloy Duran <eloy.de.enige@gmail.com>
2012-04-16 02:07:44 +0200

    Support more languages as per @showell's suggestion, with gperf for speed as per @lrz's suggestion.

--- compiler.c
+++ compiler.c
@@ -1,16 +1,144 @@
+/* C code produced by gperf version 3.0.3 */
+/* Command-line: gperf compiler.gperf  */
+/* Computed positions: -k'2' */
+
+#if !((' ' == 32) && ('!' == 33) && ('"' == 34) && ('#' == 35) \
+      && ('%' == 37) && ('&' == 38) && ('\'' == 39) && ('(' == 40) \
+      && (')' == 41) && ('*' == 42) && ('+' == 43) && (',' == 44) \
+      && ('-' == 45) && ('.' == 46) && ('/' == 47) && ('0' == 48) \
+      && ('1' == 49) && ('2' == 50) && ('3' == 51) && ('4' == 52) \
+      && ('5' == 53) && ('6' == 54) && ('7' == 55) && ('8' == 56) \
+      && ('9' == 57) && (':' == 58) && (';' == 59) && ('<' == 60) \
+      && ('=' == 61) && ('>' == 62) && ('?' == 63) && ('A' == 65) \
+      && ('B' == 66) && ('C' == 67) && ('D' == 68) && ('E' == 69) \
+      && ('F' == 70) && ('G' == 71) && ('H' == 72) && ('I' == 73) \
+      && ('J' == 74) && ('K' == 75) && ('L' == 76) && ('M' == 77) \
+      && ('N' == 78) && ('O' == 79) && ('P' == 80) && ('Q' == 81) \
+      && ('R' == 82) && ('S' == 83) && ('T' == 84) && ('U' == 85) \
+      && ('V' == 86) && ('W' == 87) && ('X' == 88) && ('Y' == 89) \
+      && ('Z' == 90) && ('[' == 91) && ('\\' == 92) && (']' == 93) \
+      && ('^' == 94) && ('_' == 95) && ('a' == 97) && ('b' == 98) \
+      && ('c' == 99) && ('d' == 100) && ('e' == 101) && ('f' == 102) \
+      && ('g' == 103) && ('h' == 104) && ('i' == 105) && ('j' == 106) \
+      && ('k' == 107) && ('l' == 108) && ('m' == 109) && ('n' == 110) \
+      && ('o' == 111) && ('p' == 112) && ('q' == 113) && ('r' == 114) \
+      && ('s' == 115) && ('t' == 116) && ('u' == 117) && ('v' == 118) \
+      && ('w' == 119) && ('x' == 120) && ('y' == 121) && ('z' == 122) \
+      && ('{' == 123) && ('|' == 124) && ('}' == 125) && ('~' == 126))
+/* The character set is not based on ISO-646.  */
+error "gperf generated tables don't work with this execution character set. Please report a bug to <bug-gnu-gperf@gnu.org>."
+#endif
+
+#line 1 "compiler.gperf"
+  /* -*- C -*- */
 #include <stdio.h>
 #include <string.h>
+
+#define TOTAL_KEYWORDS 9
+#define MIN_WORD_LENGTH 2
+#define MAX_WORD_LENGTH 7
+#define MIN_HASH_VALUE 4
+#define MAX_HASH_VALUE 14
+/* maximum key range = 11, duplicates = 0 */
+
+#ifdef __GNUC__
+__inline
+#else
+#ifdef __cplusplus
+inline
+#endif
+#endif
+static unsigned int
+hash (str, len)
+     register const char *str;
+     register unsigned int len;
+{
+  static unsigned char asso_values[] =
+    {
+      15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+      15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+      15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+      15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+      15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+      15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+      15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+      15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+      15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+      15, 15, 15, 15, 15, 15, 15,  5, 15, 10,
+      15, 15, 15, 15, 15, 15,  5, 15, 15, 15,
+      15, 15,  0, 15, 15, 15, 15, 15, 15, 15,
+      15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+      15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+      15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+      15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+      15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+      15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+      15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+      15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+      15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+      15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+      15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+      15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+      15, 15, 15, 15, 15, 15, 15, 15, 15, 15,
+      15, 15, 15, 15, 15, 15
+    };
+  return len + asso_values[(unsigned char)str[1]];
+}
+
+#ifdef __GNUC__
+__inline
+#ifdef __GNUC_STDC_INLINE__
+__attribute__ ((__gnu_inline__))
+#endif
+#endif
+const char *
+in_word_set (str, len)
+     register const char *str;
+     register unsigned int len;
+{
+  static const char * wordlist[] =
+    {
+      "", "", "", "",
+      "-php",
+      "-perl",
+      "",
+      "-pascal",
+      "-js",
+      "-ada",
+      "-java",
+      "-algol",
+      "-c",
+      "",
+      "-cpp"
+    };
+
+  if (len <= MAX_WORD_LENGTH && len >= MIN_WORD_LENGTH)
+    {
+      register int key = hash (str, len);
+
+      if (key <= MAX_HASH_VALUE && key >= 0)
+        {
+          register const char *s = wordlist[key];
+
+          if (*str == *s && !strcmp (str + 1, s + 1))
+            return s;
+        }
+    }
+  return 0;
+}
+#line 16 "compiler.gperf"
+
 
 int main (int argc, char *argv[])
 {
   if (argc != 3) {
-    printf("Usage: %s [-js|-cs] FILE\n", argv[0]);
+    printf("Usage: %s [-ada|-algol|-c|-cpp|-cs|-java|-js|-pascal|-perl|-php|-ruby] FILE\n", argv[0]);
     return 1;
   }
 
-  unsigned char javascript = 0;
-  if (strcmp(argv[1], "-js") == 0) {
-    javascript = 1;
+  unsigned char semicolon = 0;
+  if (in_word_set(argv[1], strlen(argv[1]))) {
+    semicolon = 1;
   }
 
   char *source_file = argv[2];
@@ -27,11 +155,11 @@ int main (int argc, char *argv[])
         // This is where the language translation magic happens.
         case ';':
           if (!comment) {
-            if (javascript) {
-              // JavaScript translation.
+            if (semicolon) {
+              // With semcolon translation.
               putc(';', stdout);
             } else {
-              // CoffeeScript translation.
+              // Without semicolon translation.
             }
           }
           break;
